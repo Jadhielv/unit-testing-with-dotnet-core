dd05882d6cf836dee1308d64f8c781483aa38477
Jadhiel Vélez <jadhielv@gmail.com>
2019-09-07 20:21:38 -0400

    add: writing the test in OrderHelper

--- app-test/OrderHelperTest.cs
+++ app-test/OrderHelperTest.cs
@@ -10,7 +10,41 @@ namespace app_test
         [TestMethod]
         public void ShouldSumCostOfOrder()
         {
-            
+            // Arrange  
+            var ProductCD = new Product()
+            {
+                Type = ProductType.CD,
+                Name = "Nirvana",
+                Description = "Album"
+            };
+            var ProductMovie = new Product()
+            {
+                Type = ProductType.DVD,
+                Name = "Gladiator",
+                Description = "Movie"
+            };
+
+            var order = new Order()
+            {
+                Items = new List<OrderItem>(){
+                    new OrderItem(){
+                        Quantity = 1,
+                        Price = 10,
+                        Product = ProductCD
+                    },
+                    new OrderItem(){
+                        Quantity = 2,
+                        Price = 10,
+                        Product = ProductMovie
+                    }
+                }
+            };
+
+            // Act
+            var actual = OrderHelper.Cost(order);
+
+            // Assert
+            Assert.AreEqual(30, actual);
         }
     }
 }
