a0f3a12a8b0d8a11c387ad729f8cc833ae64175a
Jadhiel Vélez <jadhielv@gmail.com>
2019-09-11 21:18:19 -0400

    add: more scenarios

--- app-test/CalculatorTest.cs
+++ app-test/CalculatorTest.cs
@@ -1,3 +1,4 @@
+using System;
 using app;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,8 +9,11 @@ namespace app_test
     {
         [DataTestMethod]
         [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
         public void Add(int value)
         {
+            Console.WriteLine("Testing {0} + {0}", value);
             var actual = Calculator.Add(value, value);
             var expected = value + value;
             Assert.AreEqual(actual, expected);
